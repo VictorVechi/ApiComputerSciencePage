a5f2f4ed50aee88cff1b4cf942dd226b2bcbc24d
EricoMeger <ericomeger9@gmail.com>
2024-10-23 12:04:52 -0300

    create post validation

--- src/infra/repository/PostRepository.ts
+++ src/infra/repository/PostRepository.ts
@@ -2,6 +2,7 @@ import { injectable } from "tsyringe";
 import { IPostSchema } from "../../domain/repository/model/IPost";
 import BaseRepository from "./BaseRepository";
 import PostModel from "./model/PostModel";
+import { Types, DeleteResult } from 'mongoose';
 
 @injectable()
 export default class PostRepository extends BaseRepository<IPostSchema> {
@@ -15,5 +16,9 @@ export default class PostRepository extends BaseRepository<IPostSchema> {
 
     async findByTag(tag: string): Promise<IPostSchema[]> {
         return await this.findAllByField({ 'tags.name': tag });
+    }
+
+    async deleteById(id: Types.ObjectId): Promise<DeleteResult | null>{
+        return await this.delete(id);
     }
 }
---
+++ src/infra/service/PostValidationService.ts
@@ -0,0 +1,79 @@
+import {inject, injectable} from 'tsyringe';
+import { DependencyEnum } from '../DependencyInjection/DependencyEnum';
+import { IPostValidationService } from '../../domain/service/IPostValidationService';
+import PostRepository from '../repository/PostRepository';
+import { Types } from 'mongoose';
+
+@injectable()
+export default class PostValidationService implements IPostValidationService {
+    constructor(
+        @inject(DependencyEnum.POST_REPOSITORY) private postRepository: PostRepository
+    ) {}
+
+    validateCreate(post_details: any): boolean {
+        if (!post_details.title) {
+            console.log('Title is required');
+            return false;
+        }
+
+        if (!post_details.content) {
+            console.log('Content is required');
+            return false;
+        }
+
+        if(!post_details.tags) {
+            console.log('At least one tag is required')
+            return false;
+        }
+
+        return true;
+    }
+
+    async validateUpdate(id: Types.ObjectId, post_details: any): Promise<boolean> {
+        if (!id) {
+            console.log('Id is required');
+            return false;
+        }
+
+        if (!post_details.title) {
+            console.log('Title is required');
+            return false;
+        }
+
+        if (!post_details.content) {
+            console.log('Content is required');
+            return false;
+        }
+
+        if(!post_details.tags) {
+            console.log('At least one tag is required')
+            return false;
+        }
+
+        const post = await this.postRepository.findById(id);
+        if(post && post._id.toString() == id){
+            return true;
+        }
+
+        console.log('Post not found');
+        return false;
+    }
+
+    async validateDelete(id: Types.ObjectId): Promise<boolean> {
+        if (!id) {
+            console.log('Id is required');
+            return false;
+        }
+
+        const post = await this.postRepository.findById(id);
+        if(post && post._id.toString() == id){
+            const delete_result = await this.postRepository.deleteById(id);
+            if(delete_result){
+                return true;
+            }
+        }
+
+        console.log('Post not found');
+        return false;
+    }
+} 
