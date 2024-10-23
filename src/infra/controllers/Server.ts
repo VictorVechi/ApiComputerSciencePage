7a4923c15c07d43f46dd19ad3a6a8562ce0f3d66
EricoMeger <ericomeger9@gmail.com>
2024-10-23 18:01:21 -0300

    add post controller to server

--- src/infra/controllers/Server.ts
+++ src/infra/controllers/Server.ts
@@ -5,6 +5,7 @@ import { IUserController } from "../../domain/controller/IUserController";
 import { DependencyEnum } from "../DependencyInjection/DependencyEnum";
 import { IRoleController } from "../../domain/controller/IRoleController";
 import { ITagController } from "../../domain/controller/ITagController";
+import { IPostController } from "../../domain/controller/IPostController";
 
 DependencyInjector.register();
 
@@ -14,9 +15,11 @@ app.use(express.json());
 const userController = container.resolve<IUserController>(DependencyEnum.USER_CONTROLLER);
 const roleController = container.resolve<IRoleController>(DependencyEnum.ROLE_CONTROLLER);
 const tagController = container.resolve<ITagController>(DependencyEnum.TAG_CONTROLLER);
+const postController = container.resolve<IPostController>(DependencyEnum.POST_CONTROLLER);
 
 roleController.routes(app);
 userController.routes(app);
 tagController.routes(app);
+postController.routes(app);
 
 export default app;
